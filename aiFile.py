4d2ab5e54172a8377056bff34812c864def5f710
Alex Larkings <145308756+DaRealElementus@users.noreply.github.com>
2024-07-26 12:28:39 +1000

    basic ai testing

--- aiFile.py
+++ aiFile.py
@@ -1,3 +1,23 @@
 '''File to utilise the CHATGPT API'''
 import openai
+import os
+from keys import openaiKey 
 
+
+openai.api_key = openaiKey
+client = openai.OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get(openaiKey),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-4-mini",
+)
+
+print(chat_completion.choices[0].text.strip())
