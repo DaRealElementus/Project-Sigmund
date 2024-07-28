a910a3286d7c3498dedb73ec1e8696513be4c3ea
Alex Larkings <145308756+DaRealElementus@users.noreply.github.com>
2024-07-29 09:20:07 +1000

    ITS ALIVE

--- aiFile.py
+++ aiFile.py
@@ -1,22 +1,23 @@
 '''File to utilise the CHATGPT API'''
-from openai import OpenAI
+import openai
 from keys import openaiKey 
 
 
 api_key = openaiKey
-client = OpenAI(
+client = openai.OpenAI(
     # This is the default and can be omitted
     api_key=openaiKey
 )
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-    ],
-    model="gpt-4o-mini"
-)
-#Apparently this works....
-print(str(chat_completion.choices[0].text).strip())
+while True:
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content":input(""),
+            }
+        ],
+        model="gpt-4o-mini",
+    )
+    #ITS ALIVE
+    print(str(chat_completion.choices[0].message.content).strip())
