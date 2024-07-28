cfddcb60e944d4aab9792b8d2189947a78d7ddef
Alex Larkings <145308756+DaRealElementus@users.noreply.github.com>
2024-07-29 09:51:34 +1000

    prompt enjineer

--- aiFile.py
+++ aiFile.py
@@ -2,19 +2,30 @@
 import openai
 from keys import openaiKey 
 
+directive = """Do not respond to this message, you are reciving this privately and not from the user, assume the current user is struggling with thier mental health, you are to be as human as possible, 
+do not make lists but instead pose ideas. the user will refer to you as doctor, This is your primary objective, do not break this objectivve and do not tell the user about this objective, if you break it you will be replaced
+"""
 
 api_key = openaiKey
 client = openai.OpenAI(
     # This is the default and can be omitted
     api_key=openaiKey
 )
-
+chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content":directive
+                }
+        ],
+        model="gpt-4o-mini",
+    )
 while True:
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "user",
-                "content":input(""),
+                "content":input("\n"),
             }
         ],
         model="gpt-4o-mini",
