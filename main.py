a2e04e0255a0259f5e9b641a75cfc98f1d462395
Alex Larkings <145308756+DaRealElementus@users.noreply.github.com>
2024-07-30 10:16:17 +1000

    AI testing + main.py intergration
    ai is now in Main
    
    ai has a catch system which prevents errors
    ai's memory can be wiped without a program restart

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 '''Main file, this is the one that is being run'''
 import aiFile
 import inputOutputSystem
-import math
 import keys
 import serial
+
+resetString = "debug_reset_EmergencyShower"
+
+ser = serial.Serial()
+ser.baudrate = 19200
+ser.port = 'COM1'
+
+while True:
+    #Funky input
+    userWords = input("Lani please work on the speech recog algo: ")
+    #^ replace with actual input func
+    if userWords == resetString:
+        aiFile.history = []
+    else:
+        output = aiFile.generate_response(userWords)
+        emotion, content = output.split(":")
+        emotion.strip()
+        # if emotion in aiFile.emotions:
+        #     emotion = str.encode(emotion)
+        #     ser.write(emotion)
+        
+        #Funky output
+        print(str(content.strip()))
+        #^replace with actual output func
