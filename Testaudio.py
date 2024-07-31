23f0dcbcb69db697e9572d3874a2f58803fcdd36
沒人知道 <147779565+Ogbeni28@users.noreply.github.com>
2024-07-31 15:05:46 +1000

    Made function recognise audio

--- Testaudio.py
+++ Testaudio.py
@@ -17,29 +17,16 @@ def SpeakText(command):
     engine.say(command) 
     engine.runAndWait()
     
-    
-# Loop infinitely for user to
-# speak
 
-while(1):    
-    
-    # Exception handling to handle
-    # exceptions at the runtime
-    
-    # use the microphone as source for input.
+def recogniseAudio():
     with sr.Microphone() as source2:
-        
-        # wait for a second to let the recognizer
-        # adjust the energy threshold based on
-        # the surrounding noise level 
+    
+   
         r.adjust_for_ambient_noise(source2, duration=0.2)
-        
-        #listens for the user's input 
+    
         audio2 = r.listen(source2)
         
-        # Using google to recognize audio
         MyText = r.recognize_google(audio2)
         MyText = MyText.lower()
-
-        print("Did you say ", MyText)
-        SpeakText(MyText)
+        return MyText
+        
