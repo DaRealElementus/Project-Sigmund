a4d9d441feff081e3b614c17771bc4987922f569
Alex Larkings <145308756+DaRealElementus@users.noreply.github.com>
2024-08-08 12:00:17 +1000

    pruning and cleaning

--- inputOutput.py
+++ inputOutput.py
@@ -14,16 +14,3 @@ def SpeakText(command):
     engine.runAndWait()
     
 
-def recogniseAudio():
-    with sr.Microphone() as source2:
-    
-   
-        r.adjust_for_ambient_noise(source2, duration=0.2)
-    
-        audio2 = r.listen(source2, timeout=5)
-        
-        MyText = r.recognize_google(audio2)
-        MyText = MyText.lower()
-        print(MyText)
-        return MyText
-        
