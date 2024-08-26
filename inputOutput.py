85a370910458b78482959c53f14b05937c9d6452
Alex Larkings <145308756+DaRealElementus@users.noreply.github.com>
2024-08-26 16:26:13 +1000

    Fixes

--- inputOutput.py
+++ inputOutput.py
@@ -1,16 +1,30 @@
 '''File to utilise the Microphone speaker system'''
 import pyttsx3 
+import pygame
+import os
 
-
+mix = pygame.mixer
+mix.init()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[12].id)
 engine.setProperty('rate', 155)
-    
+
 
 # Function to convert text to
 # speech
 def SpeakText(command):
     # Initialize the engine
-    engine.say(command) 
+    engine.save_to_file(command, "Words.wav") 
     engine.runAndWait()
+    mix.music.load("Words.wav")
+    mix.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
+    mix.music.unload()
+    if os.path.exists("Words.wav"):
+        os.remove("Words.wav")
+    else:
+        print("The file does not exist")
+
+SpeakText("hello")
